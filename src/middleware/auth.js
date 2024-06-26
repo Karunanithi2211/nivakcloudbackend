4b3de162f9e2bf1de2b799b622319d8ca087f1e5
Karunanithi2211 <kavinmano2211@gmail.com>
2024-06-26 09:55:47 +0530

    netlify

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,7 +6,7 @@ const ErrorResponse = require('../utils/errorResponse')
 exports.isAuthenticated = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
     if (!token) {
-        return res.status(401).json({ success: false, message: 'Unauthorized' });
+        return res.status(401).json({ success: false, message: 'Unauthorized'+token });
     }
 
     try {
@@ -14,6 +14,6 @@ exports.isAuthenticated = async (req, res, next) => {
         req.user = await User.findById(decoded.id);
         next();
     } catch (error) {
-        return res.status(401).json({ success: false, message: 'Unauthorized' });
+        return res.status(401).json({ success: false, message: 'Unauthorized'+token });
     }
 };
